326dfd150226ef361d6bcb56a50466a42bc889a6
Nikolay Zalutskiy <pacemkr@gmail.com>
2013-12-08 15:22:25 -0500

    CLI.

--- secrets.ml
+++ secrets.ml
@@ -1,4 +1,40 @@
 open Core.Std
 
+
+let list () =
+  print_string "Listing\n"
+
+let import file =
+  printf "Importing from file\n"
+
+let existing_file =
+  Command.Spec.Arg_type.create
+    (fun filename -> match Sys.is_file filename with
+    | `Yes -> filename
+    | `No | `Unknown ->
+      eprintf "'%s' file not found.\n%!" filename;
+      exit 1
+    )
+
+let import_command =
+  Command.basic ~summary:"Import entries from old format."
+    Command.Spec.(
+      empty
+      +> anon (maybe ("filename" %: existing_file))
+    )
+    (fun filename () -> match filename with
+    | Some(filename) -> import (In_channel.create filename)
+    | None -> import stdin
+    )
+
+let list_command =
+  Command.basic ~summary:"List stored entries."
+    Command.Spec.(empty)
+    (fun () -> print_endline "list")
+
+let commands =
+  Command.group ~summary:"Manage encrypted secrets."
+    ["import", import_command; "list", list_command]
+
 let () =
-  In_channel.iter_lines In_channel.stdin ~f:(fun l -> print_endline l)
+  Command.run ~version:"0.1.0" commands
