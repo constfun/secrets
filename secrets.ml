7bbd20a9dc0e54bb7a57784f58b605a6450f5081
Nikolay Zalutskiy <pacemkr@gmail.com>
2013-12-09 19:22:25 -0500

    Encode db as sexp and write to file.
    This commit is too big, other changes: strip whitespace from title and
    pass on import, refactor filename arg type so that it can be used to
    enforce existing OR non-existing files.

--- secrets.ml
+++ secrets.ml
@@ -15,43 +15,57 @@ end = struct
 end*)
 
 (* Ghetto way. *)
-type entry = { title : string; payload : (string * string) list; }
-type 'a db = 'a list
+type entry = { title : string; payload : (string * string) list; } with sexp
+type 'a db = 'a list with sexp
 
 let list () =
   print_string "Listing\n"
 
-let import file =
+let save db target_filename =
+  let encoded = Sexp.to_string (sexp_of_db sexp_of_entry db) in
+  Out_channel.write_all target_filename ~data:encoded
+
+let import file target_filename =
   let r = Regex.create_exn "(^[^[].*)[\\s](.*)$" in
   let db = In_channel.fold_lines file ~init:[] ~f:(fun db l ->
     match Regex.find_submatches r l with
-    | Error e -> db
+    | Error _ -> db
     | Ok submatches ->
-      let title = match submatches.(1) with Some(t) -> t | None -> assert false in
-      let password = match submatches.(2) with Some(p) -> p | None -> assert false in
+      let title = match submatches.(1) with Some(t) -> String.strip t | None -> assert false in
+      let password = match submatches.(2) with Some(p) -> String.strip p | None -> assert false in
       let entry = { title; payload=[("password", password)] } in
       entry :: db
   ) in
-  List.iter db ~f:(fun entry -> print_endline entry.title)
+  save db target_filename
 
-let existing_file =
+let filename ~should_exist =
   Command.Spec.Arg_type.create
-    (fun filename -> match Sys.is_file filename with
-    | `Yes -> filename
-    | `No | `Unknown ->
+    (fun filename -> match (Sys.is_file filename), should_exist with
+    | `Yes, true | `No, false -> filename
+    | `Yes, false ->
+        eprintf "'%s' file already exists.\n%!" filename;
+        exit 1
+    | `No, true | `Unknown, _ ->
       eprintf "'%s' file not found.\n%!" filename;
       exit 1
     )
+
+let secrets_file_flag ~should_exist =
+  Command.Spec.(
+    empty
+    +> flag "-secrets-file" (required (filename ~should_exist)) ~doc:"Location of the secrets file."
+  )
 
 let import_command =
   Command.basic ~summary:"Import entries from old format."
     Command.Spec.(
       empty
-      +> anon (maybe ("filename" %: existing_file))
+      +> anon (maybe ("filename" %: filename ~should_exist:true ))
+      ++ secrets_file_flag ~should_exist:false
     )
-    (fun filename () -> match filename with
-    | Some(filename) -> import (In_channel.create filename)
-    | None -> import stdin
+    (fun source_filename target_filename () -> match source_filename with
+    | Some(filename) -> import (In_channel.create filename) target_filename
+    | None -> import stdin target_filename
     )
 
 let list_command =
