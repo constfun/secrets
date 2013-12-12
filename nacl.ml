bf555940ad463cf4fe4fd06faaa8a2ab8bba8597
Nikolay Zalutskiy <pacemkr@gmail.com>
2013-12-12 01:14:06 -0500

    Refactor nacl secretbox.

--- nacl.ml
+++ nacl.ml
@@ -10,12 +10,19 @@ type t = char array
 
 module Secretbox = struct
   type t = {
-    arr : char array;
+    buf : char array;
     n : char ptr;
     nlen : int;
     c : char ptr;
     clen : int
   }
+
+  let create buf =
+    let n = Array.start buf in
+    let nlen = Libsodium.crypto_secretbox_NONCEBYTES in
+    let c = n +@ nlen in
+    let clen = (Array.length buf) - nlen in
+    { buf; n; nlen; c; clen }
 
   let char_ptr_of_string =
     coerce string (ptr char)
@@ -33,39 +40,30 @@ module Secretbox = struct
     let arrptr = char_ptr_of_string s in
     Array.from_ptr arrptr (String.length s)
 
-  let create arr =
-    let n = Array.start arr in
-    let nlen = Libsodium.crypto_secretbox_NONCEBYTES in
-    let c = n +@ nlen in
-    let clen = (Array.length arr) - nlen in
-    { arr; n; nlen; c; clen }
-
   let box key data =
-    let padded_m = (String.make Libsodium.crypto_secretbox_ZEROBYTES '\000') ^ data in
-    let clen = String.length padded_m in
-    let nlen = Libsodium.crypto_secretbox_NONCEBYTES in
-    let boxed = Array.make char (nlen + clen) in
-
-    let n = Array.start boxed in
-    Libsodium.randombytes_buf (to_voidp n) (Unsigned.Size_t.of_int nlen);
-
-    let c = n +@ nlen in
-    let m = char_ptr_of_string padded_m in
-    let mlen = llong_of_int clen in
     let k = char_ptr_of_string key in
-    if (Libsodium.crypto_secretbox c m mlen n k) = 0 then create boxed
+    let m = (String.make Libsodium.crypto_secretbox_ZEROBYTES '\000') ^ data in
+    let mptr = char_ptr_of_string m in
+    let mlen = String.length m in
+    let mlenLL = llong_of_int mlen in
+
+    let buflen = Libsodium.crypto_secretbox_NONCEBYTES + mlen in
+    let boxed = create (Array.make char buflen) in
+    Libsodium.randombytes_buf (to_voidp boxed.n) (Unsigned.Size_t.of_int boxed.nlen);
+
+    if (Libsodium.crypto_secretbox boxed.c mptr mlenLL boxed.n k) = 0 then boxed
     else raise Crypto_failed
 
   let box_open key boxed =
-    let padded_m = Array.make char boxed.clen ~initial:'\000' in
-    let m = Array.start padded_m in
+    let m = Array.make char boxed.clen ~initial:'\000' in
+    let mptr = Array.start m in
     let k = char_ptr_of_string key in
-    let clen = llong_of_int boxed.clen in
-    if (Libsodium.crypto_secretbox_open m boxed.c clen boxed.n k) = 0 then
-      string_of_char_array padded_m ~start:Libsodium.crypto_secretbox_ZEROBYTES
+    let clenLL = llong_of_int boxed.clen in
+    if (Libsodium.crypto_secretbox_open mptr boxed.c clenLL boxed.n k) = 0 then
+      string_of_char_array m ~start:Libsodium.crypto_secretbox_ZEROBYTES
     else raise Crypto_failed
 
-  let to_string boxed = string_of_char_array boxed.arr
+  let to_string boxed = string_of_char_array boxed.buf
 
   let of_string s = create (char_array_of_string s)
 end
