d99908b2b97627434ce40cd7a9e2f00f704b0bb3
Nikolay Zalutskiy <pacemkr@gmail.com>
2014-01-08 13:11:09 -0500

    Symlink default secrets file location.

--- cli.ml
+++ cli.ml
@@ -17,16 +17,20 @@ let filename ~should_exist =
 
 let () =
   Unix.mkdir_p ~perm:0o700 secrets_rc_path;
-  let key_path = Filename.implode [secrets_rc_path; "key"] in
+  let default_key_path = Filename.implode [secrets_rc_path; "key"] in
+  let default_sec_path = Filename.implode [secrets_rc_path; "default"] in
 
   let open Command in
   let init_cmd = basic ~summary:"Create a new secrets file."
     Spec.(empty +> anon ("path" %: filename ~should_exist:false))
     (fun path () ->
-      let key = Crypto.create key_path in
+      let key = Crypto.create default_key_path in
       Crypto.with_file path ~key:key ~f:(fun _ ->
-        Secrets.to_string (Secrets.create ())
-      ))
+        Secrets.to_string (Secrets.create ()));
+      match Sys.file_exists_exn ~follow_symlinks:false default_sec_path with
+      | false -> Unix.symlink (Filename.realpath path) default_sec_path
+      | true -> ()
+    )
   in
   run ~version:"0.1.0"
     (group ~summary:"Manage encrypted secrets." [
