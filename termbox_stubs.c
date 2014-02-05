ea99c8b3a9055235aac7fbc607a034150e147b57
Nikolay Zalutskiy <pacemkr@gmail.com>
2014-02-05 01:27:54 -0500

    Document some tricky code.

--- termbox_stubs.c
+++ termbox_stubs.c
@@ -59,23 +59,52 @@ CAMLprim value tbstub_poll_event() {
 	struct tb_event e;
 	tb_poll_event(&e);
 
+	// type event =
+	// | Key of key           -> block with tag 0
+	// | Ascii of char        -> block with tag 1
+	// | Utf8 of int32        -> block with tag 2
+	// | Resize of int * int  -> block with tag 3
+
 	if( e.type == TB_EVENT_KEY ) {
 
 		// Key
+		//
+		// We deviate from tb_event definition of key here.
+		// Some keys are really low enough to be considered ascii values.
+		//
+		// tb_poll_event reports ch as 0 whenever a 'key' is present.
 		if( e.ch == 0 && e.key > 0xFF ) {
 
 			caml_e = caml_alloc(1, 0);
+			// We use a bit of a trick here to convert e.key to
+			// type key =
+			// | F1           -> Val_int(0)
+			// ...
+			// | Arrow_right  -> Val_int(21)
+			//
+			// All (non-ascii) TB_KEY_* values are defined as (0xFFFF-0)(F1)...(0xFFFF-21)(ARROW_RIGHT).
+			// Notice the pattern ->                                  ^                ^
+			//
+			// By "restoring" that offset number we get the int value that we need to represent the variant.
 			Store_field(caml_e, 0, Val_int(0xFFFF - e.key));
 		}
 		// Ascii
+		//
+		// tb_poll_event reports key as 0 whenever a ch is present.
 		else if( e.ch <= 0xFF ) {
 
 			caml_e = caml_alloc(1, 1);
+			// Another bit of tricky code.
+			// At this point, we know that either e.key < 255 && e.ch = 0
+			//                                 or e.key = 0 && e.ch < 255
+			// So we just bitwise or the two values to get our ascii value.
 			Store_field(caml_e, 0, Val_int(e.ch | e.key));
 		}
 		// Utf8
 		else {
 
+			// All else failed, so we need to represent the ch value as an int32 block,
+			// since OCaml has no unicode support.
 			caml_e = caml_alloc(1, 2);
 			caml_ch = caml_copy_int32(e.ch);
 			Store_field(caml_e, 0, caml_ch);
