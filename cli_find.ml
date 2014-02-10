a8188b60766f17cc198d273ea97d7717fdeb5a9b
Nick Zalutskiy <nick@zalutskiy.com>
2014-02-10 16:36:30 -0500

    Copy value to clipboard.

--- cli_find.ml
+++ cli_find.ml
@@ -12,8 +12,9 @@ type state = {
   secrets : Secrets.t;
   query : string;
   selection : int;
-  results : string array;
-  results_hl : hl array
+  results : qres list;
+  summary : string array;
+  summary_hl : hl array
 }
 
 let render_cells ~pos:(x, y) ~size:(w, h) ~f =
@@ -56,21 +57,30 @@ let search secrets query =
   let len = List.length results in
   let lines = Array.create ~len "" in
   let hl = Array.create ~len (Set.empty ~comparator:Int.comparator) in
-  List.iteri results ~f:(fun i { summary; summary_hl; _ } ->
+  List.iteri results ~f:(fun i { summary; summary_hl; value } ->
     lines.(i) <- summary;
-  hl.(i) <- summary_hl);
-  (lines, hl)
+    hl.(i) <- summary_hl);
+    (results, lines, hl)
 
-let slider_cells r sel (x, y) (w, h) =
-  let rlen = Array.length r in
-  let ch = if y = sel && x = (w-1) && rlen > 0 then '>' else '\x00' in
+let slider_cells len sel (x, y) (w, h) =
+  let ch = if y = sel && x = (w-1) && len > 0 then '>' else '\x00' in
   { ch; fg=Default; bg=Default }
+
+type slider_action =
+  | Update of int
+  | Select of int
 
 let slider_input e sel h =
   match e with
-  | Key Arrow_down -> Int.min (h - 1) (sel + 1)
-  | Key Arrow_up -> Int.max 0 (sel - 1)
-  | _ -> sel
+  | Ascii c when c = '\x0D' (* ENTER *) -> Select sel
+  | Key Arrow_down -> Update (Int.min (h - 1) (sel + 1))
+  | Key Arrow_up -> Update (Int.max 0 (sel - 1))
+  | _ -> Update sel
+
+let pbcopy s =
+  let po = Unix.open_process_out "pbcopy" in
+  Out_channel.output_string po s;
+  ignore (Unix.close_process_out po)
 
 let rec loop state =
   let winw = Termbox.width () in
@@ -78,20 +88,25 @@ let rec loop state =
   let sliderh = winh - 1 in
   render_cells ~pos:(0, 0) ~size:(6, 1) ~f:(string_cells "find: ");
   render_cells ~pos:(6, 0) ~size:((winw-6), 1) ~f:(string_cells state.query);
-  render_cells ~pos:(6, 1) ~size:((winw-6), (winh-1)) ~f:(results_cells state.results state.results_hl);
-  render_cells ~pos:(0, 1) ~size:(5, sliderh) ~f:(slider_cells state.results state.selection);
+  render_cells ~pos:(6, 1) ~size:((winw-6), (winh-1)) ~f:(results_cells state.summary state.summary_hl);
+  render_cells ~pos:(0, 1) ~size:(5, sliderh) ~f:(slider_cells (List.length state.results) state.selection);
   Termbox.present ();
   let state = match Termbox.poll_event () with
     | Ascii c when c = '\x03' (* CTRL_C *) -> None
-    | (Ascii _ | Key _ | Utf8 _) as e ->
+    | (Ascii _ | Key _ ) as e ->
         (match handle_input e state.query with
         | Some query ->
-            let (results, results_hl) = search state.secrets query in
-            Some { state with query; results; results_hl; selection=0 }
+            let (results, summary, summary_hl) = search state.secrets query in
+            Some { state with query; results; summary; summary_hl; selection=0 }
         | None ->
-            let selection = slider_input e state.selection (Int.min (Array.length state.results) sliderh) in
-            Some { state with selection })
-    | Resize _ -> Some state
+            (match slider_input e state.selection (Int.min (List.length state.results) sliderh) with
+            | Update selection -> Some { state with selection }
+            | Select selection ->
+                pbcopy (List.nth_exn state.results selection).value;
+                None
+            )
+        )
+    | Utf8 _ | Resize _ -> Some state
   in
   match state with
   | Some s -> loop s
@@ -99,5 +114,5 @@ let rec loop state =
 
 let start secrets =
   ignore (Termbox.init ());
-  loop { secrets; query=""; selection=(-1); results=[||]; results_hl=[||] };
+  loop { secrets; query=""; selection=(-1); results=[]; summary=[||]; summary_hl=[||] };
   Termbox.shutdown ()
