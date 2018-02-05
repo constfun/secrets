5e24cd63331ad0cee58d2c9875598c11ed567822
Nick Zalutskiy <nick@const.fun>
2018-02-04 21:25:57 -0500

    Hello Cairo.

--- cli.ml
+++ cli.ml
@@ -144,9 +144,43 @@ module AddView = struct
 end
 
 let add = with_secrets_file ~f:(fun sec ->
-    let term = Notty_unix.Term.create () in
-    let view = AddView.create () in
-    AddView.init view term;
+    let open Cairo in
+    let pi2 = 8. *. Float.atan 1. in
+
+    let draw cr width height =
+      let r = 0.25 *. width in
+      set_source_rgba cr 0. 1. 0. 0.5;
+      arc cr (0.5 *. width) (0.35 *. height) r 0. pi2;
+      fill cr;
+      set_source_rgba cr 1. 0. 0. 0.5;
+      arc cr (0.35 *. width) (0.65 *. height) r 0. pi2;
+      fill cr;
+      set_source_rgba cr 0. 0. 1. 0.5;
+      arc cr (0.65 *. width) (0.65 *. height) r 0. pi2;
+      fill cr
+    in
+
+    let expose drawing_area ev =
+      let cr = Cairo_gtk.create drawing_area#misc#window in
+      let allocation = drawing_area#misc#allocation in
+      draw cr (float allocation.Gtk.width) (float allocation.Gtk.height);
+      true in
+
+    (* let sur = Cairo.Image.create Cairo.Image.ARGB32 100 100 in *)
+    ignore(GMain.init());
+
+    let w = GWindow.window ~title:"esns" ~width:500 ~height:400 () in
+    ignore(w#connect#destroy GMain.quit);
+
+    let d = GMisc.drawing_area ~packing:w#add () in
+    ignore(d#event#connect#expose (expose d));
+
+    w#show();
+    GMain.main();
+
+    (* let term = Notty_unix.Term.create () in *)
+    (* let view = AddView.create () in *)
+    (* AddView.init view term; *)
     sec
   )
 
